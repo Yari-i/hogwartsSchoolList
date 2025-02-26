84bffe1aaab4dd70a60480547f7003594a0135e1
Yari <tipo47181@gmail.com>
2025-02-26 19:25:00 +0500

    Added SQL and unit tests

--- src/main/java/ru/hogwarts/school/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/impl/FacultyServiceImpl.java
@@ -1,50 +1,60 @@
 package ru.hogwarts.school.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.service.FacultyService;
 
-import java.util.HashMap;
 import java.util.List;
 
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
 
-    private final HashMap<Long, Faculty> faculties = new HashMap<>();
-    private long idCount = 0;
+    @Autowired
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
 
     @Override
     public Faculty addFaculty(Faculty faculty) {
-        faculty.setId(idCount++);
-        faculties.put(faculty.getId(), faculty);
-        return faculty;
+        return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty findFaculty(long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Faculty editFaculty(long id, Faculty faculty) {
-        if (!faculties.containsKey(id)) {
-            return null;
-        }
-        faculties.put(id, faculty);
-        return faculty;
+        Faculty facultyFromDb = facultyRepository.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
+
+        facultyFromDb.setName(faculty.getName());
+        facultyFromDb.setColor(faculty.getColor());
+
+        return facultyRepository.save(facultyFromDb);
     }
 
     @Override
     public void deleteFaculty(long id) {
-        faculties.remove(id);
+        facultyRepository.deleteById(id);
     }
 
     @Override
     public List<Faculty> getAllByColor(String color) {
-        return faculties.values()
+        return facultyRepository.findAll()
                 .stream()
                 .filter(it -> it.getColor().equals(color))
                 .toList();
     }
+
+//    @Override
+//    public List<Faculty> findByNameIgnoreCaseOrColorIgnoreCase(String name, String color) {
+//        return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(name, color);
+//    }
 }
---
+++ src/test/java/ru/hogwarts/school/impl/StudentServiceImplTest.java
@@ -0,0 +1,113 @@
+package ru.hogwarts.school.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceImplTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentServiceImpl studentService;
+
+    @Test
+    void shouldCorrectlyAddStudent() {
+
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Harry Potter");
+        student.setAge(18);
+
+        when(studentRepository.save(student)).thenReturn(student);
+
+        Student savedStudent = studentService.addStudent(student);
+
+        assertEquals(student, savedStudent);
+    }
+
+    @Test
+    void shouldCorrectlyFindStudent() {
+        long id = 1;
+        Student student = new Student();
+        student.setId(id);
+        student.setName("Harry Potter");
+        student.setAge(18);
+
+        when(studentRepository.findById(id)).thenReturn(java.util.Optional.of(student));
+
+
+        Student foundStudent = studentService.findStudent(id);
+
+
+        assertEquals(student, foundStudent);
+    }
+
+    @Test
+    void shouldCorrectlyEditeStudent() {
+        long id = 1;
+        Student student = new Student();
+        student.setId(id);
+        student.setName("Harry Potter");
+        student.setAge(20);
+
+        Student studentFromDb = new Student();
+        studentFromDb.setId(id);
+        studentFromDb.setName("Old Name");
+        studentFromDb.setAge(22);
+
+        when(studentRepository.findById(id)).thenReturn(Optional.of(studentFromDb));
+        when(studentRepository.save(studentFromDb)).thenReturn(student);
+
+
+        Student editedStudent = studentService.editeStudent(id, student);
+
+
+        assertEquals(student.getName(), editedStudent.getName());
+        assertEquals(student.getAge(), editedStudent.getAge());
+
+        verify(studentRepository).save(studentFromDb);
+    }
+
+    @Test
+    void shouldCorrectlyDeleteStudent() {
+        long id = 1;
+
+        studentService.deleteStudent(id);
+
+        verify(studentRepository).deleteById(id);
+    }
+
+    @Test
+    void shouldCorrectlyGetAllByAge() {
+
+        int age = 20;
+        List<Student> allStudents = new ArrayList<>();
+        allStudents.add(new Student(1, "Harry Potter", 20));
+        allStudents.add(new Student(2, "Ron Weasley", 22));
+        allStudents.add(new Student(3, "Hermione Granger", 20));
+
+        when(studentRepository.findAll()).thenReturn(allStudents);
+
+
+        List<Student> studentsByAge = studentService.getAllByAge(age);
+
+
+        assertEquals(2, studentsByAge.size());
+        studentsByAge.forEach(student -> assertEquals(age, student.getAge()));
+    }
+}
